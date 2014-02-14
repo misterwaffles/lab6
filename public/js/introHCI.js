8bb5a3eb39b9f809a041c2faddae948fa6ce1697
Pavel Bozdog <pbozdog@ucsd.edu>
2014-02-13 17:27:36 -0800

    finished lab 6

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -3,6 +3,13 @@
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
 	initializePage();
+
+/*
+	$(".project").click(function(e) {
+	var projectID = $(this).closest('.project').attr('id');
+
+	$.get("/project/projectID", addProjectDetails);
+	})*/
 })
 
 /*
@@ -27,6 +34,22 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + idNumber, loadResult);
+	console.log("getting /project/" + idNumber);
+}
+
+function loadResult(e) {
+	console.log(e);
+	console.log("e id number = " + e['id']);
+	var temp = "#project" + e['id'] + " .details"
+	console.log(temp);
+
+	var content = '<img src="' + e['image'] + '" class="detailsImage">' + 
+			' <h3>' + e['date'] + '</h3>' + e['summary'];
+
+	$(temp).html(content);
+
 }
 
 /*
@@ -35,4 +58,18 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	//var colors = e['colors'];
+	$.get("/palette", getColors);
+}
+
+function getColors(e) {
+	console.log("in get colors method");
+	var colors = e['hex'];
+	console.log(colors[0]);
+	
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
